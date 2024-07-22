902442329b1309346aa2a0a2009c3d8ca84e4266
Dhivya2528 <ddhivyabharathi192@gmail.com>
2024-07-22 17:32:34 +0530

    First push

---
+++ variables.js
@@ -0,0 +1,27 @@
+const uid = "abc123";
+/* 
+const:
+    const is a keyword used for variable declaration in JavaScript (ES6 and beyond), 
+    creating block-scoped variables that cannot be reassigned once initialized.
+*/
+
+var fullName = "Javascript";
+var email = "js@gmail.dev";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fullname = prompt("Enter your name");
+
+console.log(uid);
+console.log("Full Name is: ", fullName);
+console.log(email);
+
+console.log(`
+    With Unique ID: ${uid}
+    User is : ${fullName}
+    and his email is: ${email}
+    and uses the password: ${password}
+
+`);
